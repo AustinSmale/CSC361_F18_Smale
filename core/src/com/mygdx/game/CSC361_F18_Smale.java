9bd8926dca8ac0b1eb55feded850e523770be884
Austin Smale <austinsmale360@gmail.com>
2018-10-31 10:13:12 -0400

    initial classes from chapter 3

--- core/src/com/mygdx/game/CSC361_F18_Smale.java
+++ core/src/com/mygdx/game/CSC361_F18_Smale.java
@@ -1,33 +1,65 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.game.WorldController;
+import com.mygdx.game.game.WorldRenderer;
 
 public class CSC361_F18_Smale extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	private static final String TAG = CSC361_F18_Smale.class.getName();
+
+	private WorldController worldController;
+	private WorldRenderer worldRenderer;
+	private boolean paused;
+
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void create() {
+		// Set Libgdx log level to DEBUG
+		Gdx.app.setLogLevel(Application.LOG_DEBUG);
+		// Initialize controller and renderer
+		worldController = new WorldController();
+		worldRenderer = new WorldRenderer(worldController);
+		// Game world is active on start
+		paused = false;
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+	public void render() {
+		// Do not update game world when paused
+		if (!paused) {
+			// Update game world by the time that has passed
+			// since last rendered frame.
+			worldController.update(Gdx.graphics.getDeltaTime());
+		}
+		// Sets the clear green screen color to: Cornflower Blue
+		Gdx.gl.glClearColor(0x64 / 255.0f, 0x95 / 255.0f, 0xed / 255.0f, 0xff / 255.0f);
+		// Clears the screen
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		// Render game world to screen
+		worldRenderer.render();
 	}
-	
+
 	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void resize(int width, int height) {
+		worldRenderer.resize(width, width);
+	}
+
+	@Override
+	public void pause() {
+		paused = true;
+	}
+
+	@Override
+	public void resume() {
+		paused = false;
+	}
+
+	@Override
+	public void dispose() {
+		worldRenderer.dispose();
 	}
 }
